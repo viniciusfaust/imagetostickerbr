c9697f01c547901b3e2e020fb35e99f8ceb77aad
viniciusfaust <72894761+viniciusfaust@users.noreply.github.com>
2020-10-14 22:38:42 -0300

    Add files via upload

---
+++ handler/message/text/id.js
@@ -0,0 +1,76 @@
+exports.textTnC = () => {
+    return `
+Créditos ao YogaSakti por criar a script, Créditos ao MrStrang por traduzir e modificar`
+}
+
+exports.textMenu = (pushname) => {
+    return `
+Olá, ${pushname}! 👋️
+Aqui estão alguns dos recursos do MrStrang-BOT!✨
+Criador de figurinhas:
+1. *#sticker*
+Para transformar a imagem em um adesivo. 
+Uso: envie uma foto com uma legenda #sticker ou responda a uma imagem enviada #sticker
+2. *#sticker* _<Url Gambar>_
+Para alterar a imagem de url para adesivo. 
+Usar: #sticker
+3. *#gifsticker* _<Giphy URL>_ / *#stickergif* _<Giphy URL>_
+Para converter gif em adesivo (Somente Giphy)
+Uso: Enviar mensagens no formato *gifsticker https://media.giphy.com/media/JUvI2c1ddyzkwK4RlV/giphy.gif*
+4. *#memesticker* _<teks atas>_ | _<teks bawah>_
+Para criar adesivos de meme com texto superior e inferior
+Uso: envie uma foto com a legenda _*#meme texto para cima | texto para baixo*_, ou você também pode responder a uma imagem existente.
+Downloader:
+1. *#tiktok* _<tiktok url>_
+Para baixar um vídeo de um vídeo do TikTok.
+Uso: envie mensagens em *#tiktok https://www.tiktok.com/@itsandani/video/6869248690381425922*
+2. *#fb* _<post/video url>_
+Para baixar vídeos do Facebook.
+Uso: enviar mensagens em formato *#fb https://www.facebook.com/.....*
+3. *#ig* _<instagram post url>_
+Para baixar fotos e vídeos do instagram.
+Uso: enviar mensagens no formato *#ig https://www.instagram.com/p/BPOd1vhDMIp/*
+4. *#twt* _<twitter post url>_
+Para baixar fotos e vídeos do Twitter.
+Uso: enviar mensagens no formato *#twt https://twitter.com/ntsana_/status/1306108656887324672*
+Outros:
+1. *#translate* _<kode bahasa>_
+Para interpretar uma mensagem em um idioma especificado.
+Uso: Responder/citar/responder a mensagem que deseja traduzir com _*#translate id*_ <- id é o código de idioma. código de idioma pode ser visualizado em *https://bit.ly/33FVldE*
+2. *#resi* _<kurir>_ _<nomer resi>_
+Para verificar o status da entrega das mercadorias, registre-se no correio: jne, post, tiki, passeios, jnt, rpx, sap, sicepat, pcp, jet, dse, first, ninja, lion, idl, rex.
+Uso: enviar mensagens em formato _*#resi jne 1238757632*_
+3. *#meme* _<teks atas>_ | _<teks bawah>_
+Para criar um meme com texto superior e inferior
+Uso: enviar imagem com legenda _*#meme texto de cima | texto de baixp*_, ou também marcando a uma imagem existente.
+4. *#ceklokasi*
+Cek lokasi penyebaran covid-19 di daerah sekitarmu (kelurahan).
+Penggunaan: kirimkan lokasimu lalu balas/quote/reply lokasi yang kamu kirim dengan _*#ceklokasi*_
+5. *#tnc*
+Ver termo de contrato do bot.
+6. *#donasi*
+Doar dinheiro.
+Tenha um ótimo dia!✨`
+}
+
+exports.textAdmin = () => {
+    return `
+⚠ [ *Admin Group Only* ] ⚠ 
+AQUI ESTÃO ALGUNS RECUROSOS DE ADMINISTRAÇÃO PARA GRUPOS DO BOT!
+1. *#kick* @user
+Banir membro (Pode ser mais de 1).
+2. *#promote* @user
+Promover membro a adm.
+3. *#demote* @user
+Tirar o cargo de adm de alguem.
+4. *#tagall*
+Marcar todos os membros do grupo (Não funciona)
+5. *#del*
+Deletar mensagem do bot (balas pesan bot dengan #del)
+*Entre em contanto com mr.strang via email*: strangm58@gmail.com`
+}
+
+exports.textDonasi = () => {
+    return `
+email no mercado pago: thaisousa777@gmail.com`
+}
